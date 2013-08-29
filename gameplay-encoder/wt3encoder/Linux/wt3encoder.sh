5bab09eb45cafb190561a37ab9078078e01b8e73
Chris E <christian.ebner@wikitude.com>
2013-08-29 11:13:14 +0200

    Updates wt3encoder scripts - add return value and options

--- gameplay-encoder/wt3encoder/Linux/wt3encoder.sh
+++ gameplay-encoder/wt3encoder/Linux/wt3encoder.sh
@@ -1,5 +1,35 @@
 #!/bin/sh
 
+set -e
+
+usage() {
+cat <<EOF
+
+Usage: 	$0 [-i model_inupt_file] [-o model_output_file]
+Encodes .FBX or .DAE 3d model file to .WT3
+
+OPTIONS:
+    -i  Filepath to the model file (e.g. "/home/username/teapot/teapot.fbx").
+    -o  Filepath to the wt3 output file  (e.g. "/home/username/teapot/teapot.wt3").
+    -h  displays this usage text
+
+EOF
+}
+
+while getopts hi:o: OPTION
+do
+   case $OPTION in
+   		h) 	usage
+		   	exit 1;;
+   		i) IN_ARG=`basename $OPTARG`;;
+		o) OUT_ARG="$OPTARG";;
+		\?)	usage
+		     exit 1;;
+		*)	usage
+		    exit 1;;
+   esac
+done
+
 #save current path
 export ORIG_PATH=$PWD
 export EXEC_PATH=`readlink -f --canonicalize  $(dirname $0)`
@@ -7,11 +37,11 @@ export EXEC_PATH=`readlink -f --canonicalize  $(dirname $0)`
 # sh Wikitude3dEncoder.sh <ABSOLUTE_MODELPATH> <OUTPUT_NAME> <OUTPUT_PATH>
 # example: 
 # sh Wikitude3dEncoder.sh ./Documents/butterfly/butterfly.fbx ./Documents/out/butterfly.wt3
-export ROOT_MODEL_PATH=`readlink -f --canonicalize $(dirname $1)`
-export MODEL_FILENAME=`basename $1`
+export ROOT_MODEL_PATH=`readlink -f --canonicalize $(dirname $IN_ARG)`
+export MODEL_FILENAME=`basename $IN_ARG`
 export MODEL_FILEPATH="$ROOT_MODEL_PATH/$MODEL_FILENAME"
-export OUT_FILENAME=`basename $2`
-export OUT_PATH=`readlink -f --canonicalize $(dirname $2)`
+export OUT_FILENAME=`basename $OUT_ARG`
+export OUT_PATH=`readlink -f --canonicalize $(dirname $OUT_ARG)`
 export OUT_FILEPATH="$OUT_PATH/$OUT_FILENAME"
 
 # create missing directories
